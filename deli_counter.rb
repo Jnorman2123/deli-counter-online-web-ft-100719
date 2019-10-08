b40b16d11417ac5089ea13fb91fe858e90a164aa
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 22:51:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 
 def line(array)
   line_string = "The line is currently"
+  final_string
   if array.length < 1
     puts line_string + " empty."
   else
     array.each_with_index do |name, number|
-      "#{number}. "
+      final_string = line_string << ": #{number}. #{name}."
     end
   end
 end
