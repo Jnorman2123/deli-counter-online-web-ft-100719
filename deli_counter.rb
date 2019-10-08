ca8373eb5ef72fdb7f92a9dd276096000eecc729
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 23:31:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,8 @@ end
 def take_a_number(array, name)
   if array.length < 1
     array << "#{name}"
-    puts "Welcome, #{name}. You are number #{array.index("#{name + 1}")} in line."
+    puts "Welcome, #{name}. You are number #{array.index("#{name} + 1")} in line."
   else
-    puts "Welcome, #{name}. You are number #{array.index("#{name + 1}")} in line."
+    puts "Welcome, #{name}. You are number #{array.index("#{name} + 1")} in line."
   end
 end
