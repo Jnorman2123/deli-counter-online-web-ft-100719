5ce2a3203669961bab18ce23376197f473fc5ef3
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 22:58:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 
 def line(array)
-  line_string = "The line is currently"
+  line_string 
   if array.length < 1
-    puts line_string + " empty."
+    puts "The line is currently empty."
   else
     array.each_with_index do |name, number|
-      line_string << ": #{number}. #{name}."
+      line_string = ": " << "#{number + 1}. #{name}."
     end
     puts line_string
   end
