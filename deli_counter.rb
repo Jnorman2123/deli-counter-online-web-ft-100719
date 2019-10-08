fc1abef2842f215c723ec6bc3c8e4e1bf28eeac7
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 22:04:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,6 @@
-# Write your code here.
+
+def line(array)
+  array.each_with_index do |name, number|
+    name == false ? p "The line is currently empty." : p "Welcome, #{name}. You are number #{number} in line."
+  end
+end
